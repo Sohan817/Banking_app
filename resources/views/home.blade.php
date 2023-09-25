cb06d12ebc522ce2c2bb81b4b5f2381dcb4fda68
Sohan817 <hsohan509@gmail.com>
2023-09-26 04:22:08 +0600

    Dropdown problem solved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,52 +1,45 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+</script>
 @extends('layouts.app')
 
 @section('content')
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <div class="container text-center">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Banking System</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Status</a>
-                        </li>
-                        <li class="nav-item mx-3">
-                            <button class="btn btn-primary">Diposit</button>
-                        </li>
-                        <li class="nav-item">
-                            <button class="btn btn-primary">Withdraw</button>
-                        </li>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Banking System</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Statement</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Diposit</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Withdraw</a>
+                    </li>
 
-                    </ul>
-
+                </ul>
+                <div class="d-flex mx-4">
+                    <button class="btn btn-outline-success" type="submit">Balence: 0.00</button>
                 </div>
-                <div class="d-flex">
-                    <button class="btn btn-outline-success" type="submit">Balence:0.00</button>
-                    <ul>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                {{ Auth::user()->name }}
-                                <a href="/logout">Logout</a>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                            </ul>
-                        </li>
+                <div class="dropdown">
+                    <button class="btn btn-sm btn-outline-info" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        {{ Auth::user()->name }}
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="/logout">Logout</a></li>
                     </ul>
                 </div>
-
             </div>
-        </nav>
-    </div>
-
+    </nav>
     <div class="container text-center">
         <div style="font-size:clamp(0rem, 1rem + 7vw, 15rem)">
             Banking System
